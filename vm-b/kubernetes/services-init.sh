48fb03f89af3aba8466fe317da604290bb32fbad
fernandorego <fernandoluisrego@gmail.com>
2023-06-01 16:44:24 +0100

    Add crab rave movie as a service

--- vm-b/kubernetes/services-init.sh
+++ vm-b/kubernetes/services-init.sh
@@ -1,6 +1,10 @@
 #! /bin/bash
 
-for file in services/*; do 
+docker build ./services/nginx-crab/ -t movie/crab
+
+kind load docker-image movie/crab
+
+for file in services/*.yaml; do 
     if [ -f "$file" ]; then 
         kubectl apply -f $file 
     fi 
